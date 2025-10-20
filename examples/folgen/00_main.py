81e6c2d0c438ee19a8ac72a3ed2c494a51a11f62
Bernhard Bachmann <bernhard.bachmann@hsbi.de>
2025-10-20 15:26:12 +0200

    Schablone für die interaktive Analyse von Folgen

--- examples/folgen/00_main.py
+++ examples/folgen/00_main.py
@@ -1,20 +1,23 @@
 
 import sympy as sp
-import matplotlib.pyplot as plt
-
-import bsp_01_grundlagen_folgen as grundlagen
-import bsp_02_konvergenz_visualisieren as epsilon_band
 
 # Importiert die 'FolgenBibliothek' aus dem Paket 'analysis_lib'
 from analysis_lib.folgen import FolgenBibliothek
 
-#grundlagen.beispiel_alternierende_harmonische_folge()
-#epsilon_band.beispiel_epsilon_band_kriterium()
-
-plt.ioff()  # Deaktiviert den interaktiven Modus
-
 n = sp.symbols('n')
 folge = (n**2 + 3*n -1)/(2*n**2+1)
 
+print("Symbolische Tabelle der Folge ")
+FolgenBibliothek.print_symbolic_sequence_table(folge, n, 1, 10)
+
+print("Numerische Tabelle der Folge ")
+FolgenBibliothek.print_numeric_sequence_table(folge, n, 1, 10)
+
+print("Plot für die Folge ")
+FolgenBibliothek.plot_indexwert(folge, n, n_start=1, n_end=200)
+
+print("Plot für die Folge ")
+FolgenBibliothek.plot_xy(folge, n, n_start=1, n_end=200)
+
 print("Plot für die Folge mit Epsilon-Band-Kriterium")
-FolgenBibliothek.plot_convergence_auto(folge, n, n_start=1, n_end=50, epsilon=0.05)
+FolgenBibliothek.plot_convergence_auto(folge, n, n_start=1, n_end=200, epsilon=0.02)
